14626b10ce384dcebb0e60860283ee6c7ff38b82
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2025-01-20 04:28:30 +0400

    add pg configurations

--- dance_project/settings.py
+++ dance_project/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-st^p2hs&x@300_rxi)m*$d8to3-h)b-g0&k!i-jobv&%ihf#)6'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,13 +94,25 @@ WSGI_APPLICATION = 'dance_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("name"),  # Database name
+        'USER': os.getenv("username"),    # Username
+        'PASSWORD': os.getenv("password"),
+        'HOST': os.getenv("host"),  # Database host
+        'PORT': os.getenv("port"),  # Port
+        'OPTIONS': {
+            'sslmode': os.getenv("sslmode"),  # Enforce SSL for secure connection
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
